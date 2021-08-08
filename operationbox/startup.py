c3478d683635a28da9dc5cf1607d940ab904e783
Daniel Song <dsong.chan@gmail.com>
2021-08-08 16:14:10 -0700

    Add wifi status led code

--- operationbox/startup.py
+++ operationbox/startup.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import os, time, re, json, ifcfg, signal
+import flask_server
+import RPi.GPIO as GPIO
+
+
 
 def quit_Greg(*args):
     print('Stopping Greg System')
@@ -9,11 +13,30 @@ def quit_Greg(*args):
 with open('system_info.json') as f:
     sys_info = json.loads(f.read())['system_INFO']
 
+wifiLED = sys_info["LowerTray"]["wifiLED"]
+# Setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(wifiLED, GPIO.OUT)
+
+def blink3_wifi():
+    for _ in range(3):
+        GPIO.output(wifiLED, GPIO.HIGH)
+        time.sleep(0.3)
+        GPIO.output(wifiLED, GPIO.LOW)
+    time.sleep(1)
+
+def blink1_wifi():
+    GPIO.output(wifiLED, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(wifiLED, GPIO.LOW)
+    time.sleep(1)
+
 def apply_wifi_creds():
     results = False
     try:
         while not os.path.exists('/media/usb0/credentials.json'):
-            time.sleep(1)
+            blink1_wifi()
         with open('/media/usb0/credentials.json') as f:
             creds = json.loads(f.read())
         with open('/etc/wpa_supplicant/wpa_supplicant.conf') as wpa:
@@ -40,18 +63,22 @@ def check_wifi(timeout=30):
     limit = 0
     while 'wlan0' not in ifcfg.interfaces().keys():
         # Blink BLUE LED HERE
+        blink3_wifi()
         print('waiting for WLAN0 interface')
         if limit == timeout:
             return False
         limit = limit+1
-        time.sleep(1)
     ### Don't check for now ####
     # if sys_info['staticIP'] not in ifcfg.interfaces()['wlan0']['inet4']:
     #     return False
     return True
 
+
+
+
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, quit_Greg)
+    GPIO.output(wifiLED, GPIO.LOW)
     # Look for WiFi
     try:
         for x in range(3):
@@ -63,5 +90,7 @@ if __name__ == '__main__':
                 break
             print("Failed to apply wifi credentials\n TRYING AGAIN")
         print("Connected to WiFi: Starting Greg Monitoring and Care System")
+        GPIO.output(wifiLED, GPIO.HIGH)
+        flask_server()
     except KeyboardInterrupt:
         quit_Greg()
